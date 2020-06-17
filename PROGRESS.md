1a228c815ff116d624d5c9ba531ccc2a98a4aa8e
Yu IHARA <ihara@ricos.co.jp>
2020-06-17 13:58:08 +0900

    Split README and work progress.

---
+++ PROGRESS.md
@@ -0,0 +1,3 @@
+# Daily my work progress
+..
+
--- push.sh
+++ push.sh
@@ -5,8 +5,8 @@ COUNT=$(($LENGTH*3/4 + 1))
 CHAR1=`dd if=/dev/urandom bs=1 count=$COUNT 2>/dev/null | base64 -w0 | cut -c -$LENGTH`
 CHAR2=`dd if=/dev/urandom bs=1 count=$COUNT 2>/dev/null | base64 -w0 | cut -c -$LENGTH`
 
-echo $CHAR1 >> README.md
-/bin/git add README.md
+echo $CHAR1 >> PROGRESS.md
+/bin/git add PROGRESS.md
 /bin/git commit -m $CHAR2
 /bin/git push origin master
 
